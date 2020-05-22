93136b0e55ef7c99ae01b35d642548efb0645927
Glenn Drescher <GlennD@me.com>
2020-05-22 13:55:39 +0200

    Add DetailView

---
+++ HWS-Classic-Project01-StormViewer/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  HWS-Classic-Project01-StormViewer
+//
+//  Created by Glenn Drescher on 22/05/2020.
+//  Copyright © 2020 Glenn Drescher. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var ImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
